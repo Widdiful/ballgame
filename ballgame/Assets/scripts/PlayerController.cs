d102adf1b70ba98884d4020baa846f14b57a7fbb
Widdiful <widdifulsa@gmail.com>
2017-02-23 22:26:20 +0000

    Version 0.01

--- ballgame/Assets/scripts/PlayerController.cs
+++ ballgame/Assets/scripts/PlayerController.cs
@@ -2,28 +2,107 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
+
+    public float inputType;
 	public float speed;
     public float rotspeed;
-    public GameObject cameraArm;
+    public float camspeed;
+    public bool enableY;
+    public bool invertX;
+    public bool invertY;
+    public bool lockCannonRotation;
+    public GameObject originPoint;
+    public GameObject cameraPitch;
+    public GameObject cameraYaw;
+    public GameObject cannon;
+    public GameObject projectile;
 	
 	private Rigidbody rb;
 	
 	void Start ()
 	{
-		rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 	}
 	
+    void Update() {
+        originPoint.transform.position = transform.position;
+
+        //Shooting
+        if (Input.GetButtonDown("Fire1")) {
+            Vector3 spawnposition = new Vector3(cannon.transform.position.x, cannon.transform.position.y + 0.9f, cannon.transform.position.z);
+            Rigidbody instantiatedProjectile = Instantiate(projectile, spawnposition, cameraPitch.transform.rotation) as Rigidbody;
+            print(rb.velocity);
+            instantiatedProjectile.velocity = rb.velocity;
+        }
+    }
+
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-        float rotation = Input.GetAxis("Horizontal");
+		//Set rotation values
+        float ballX = 0;
+        float ballY = Input.GetAxis("MoveUp");
+        float camX = 0;
+        float camY = 0;
+        float strafe = 0;
+        if (enableY == true)
+        {
+            camY = Input.GetAxis("CamY");
+        }
+        if (invertX == true)
+        {
+            camX *= -1;
+        }
+        if (invertY == true)
+        {
+            camY *= -1;
+        }
+        if (inputType == 1) {
+            ballX = Input.GetAxis("Horizontal");
+            camX = Input.GetAxis("CamX");
+        }
+        if (inputType == 2) {
+            ballX = Input.GetAxis("CamX");
+            strafe = Input.GetAxis("Horizontal");
+        }
 
-        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);
-        transform.Rotate(0, 0, rotation * rotspeed);
-        //Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        //Set rotations
+        originPoint.transform.Rotate(0 * rotspeed, ballX * rotspeed, 0);
+        if (lockCannonRotation == true && inputType == 1) {
+            cameraYaw.transform.Rotate(0 * rotspeed, (ballX * -1) * rotspeed, 0);
+        }
+        transform.rotation = originPoint.transform.rotation;
+        transform.Rotate(-90, 0, 0);
+        cannon.transform.rotation = cameraPitch.transform.rotation;
+        cannon.transform.Rotate(-90, 0, 0);
 
-        rb.AddForce (Input.GetAxis ("Vertical") * cameraArm.transform.forward * speed);
-	}
+        //Add force in forward facing direction
+        rb.AddForce(Input.GetAxis ("Vertical") * originPoint.transform.forward * speed);
+        rb.AddForce(strafe * originPoint.transform.right * speed);
+        rb.AddForce(ballY * originPoint.transform.up * speed);
+
+        //Braking
+        if(Input.GetAxis("Brake") < -0.5) {
+            rb.velocity = rb.velocity * 0.95f;
+        }
+
+        //Camera rotation
+        cameraYaw.transform.Rotate(0, camX * camspeed, 0);
+        if(cameraPitch.transform.eulerAngles.x <= 30 || cameraPitch.transform.eulerAngles.x >= 330) {
+            cameraPitch.transform.Rotate(camY * camspeed, 0, 0);
+        }
+        if (cameraPitch.transform.eulerAngles.x >= 30 && cameraPitch.transform.eulerAngles.x <= 320) {
+            cameraPitch.transform.localEulerAngles = new Vector3(29.999f,
+            cameraPitch.transform.localEulerAngles.y,
+            cameraPitch.transform.localEulerAngles.z
+            );
+        }
+        if (cameraPitch.transform.eulerAngles.x >= 40 && cameraPitch.transform.eulerAngles.x <= 330) {
+            cameraPitch.transform.localEulerAngles = new Vector3(331.001f,
+            cameraPitch.transform.localEulerAngles.y,
+            cameraPitch.transform.localEulerAngles.z
+            );
+        }
+    }
 }
---
+++ ballgame/Assets/scripts/bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+    public int speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        Vector3 direction = new Vector3(transform.forward.x, transform.forward.y, transform.forward.z);
+        rb.AddForce(direction * speed);
+        Destroy(gameObject, 5 - (speed / 1000));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
