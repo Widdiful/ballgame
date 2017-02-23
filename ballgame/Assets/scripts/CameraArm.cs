b44dac9ac2a40e9a733467883c12343972c59004
Widdiful <widdifulsa@gmail.com>
2017-02-23 13:32:36 +0000

    Initial Commit

---
+++ ballgame/Assets/scripts/CameraArm.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraArm : MonoBehaviour {
+
+    public GameObject target;
+    public int rotationspeed;
+    public bool enableY;
+    public bool invertX;
+    public bool invertY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.transform.position;
+	}
+
+    void FixedUpdate() {
+        float rotateX = Input.GetAxis("CamX");
+        float rotateY = 0;
+        if (enableY == true) {
+            rotateY = Input.GetAxis("CamY");
+        }
+        if (invertX == true) {
+            rotateX *= -1;
+        }
+        if (invertY == true)
+        {
+            rotateY *= -1;
+        }
+
+        transform.Rotate(rotateY * rotationspeed, rotateX * rotationspeed, 0);
+    }
+}
---
+++ ballgame/Assets/scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+    public float rotspeed;
+    public GameObject cameraArm;
+	
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);
+        transform.Rotate(0, 0, rotation * rotspeed);
+        //Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (Input.GetAxis ("Vertical") * cameraArm.transform.forward * speed);
+	}
+}
